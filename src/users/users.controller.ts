1761631707acf99d6342ec618b947dc36de58e46
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-11-30 00:34:22 -0300

    feat: add user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,58 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Res,
+  HttpStatus,
+  Post,
+  Body,
+  Put,
+  NotFoundException,
+  Patch,
+  Param,
+  Query,
+  Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto, UpdateUserDto } from './dto';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
+  constructor(private usersService: UsersService) {}
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  public async getAllUsers(
+    @Res() res,
+    @Query() paginationQuery: PaginationQueryDto) {
+    const users = await this.usersService.findAll(paginationQuery);
+    return res.status(HttpStatus.OK).json(users);
+  }
+  
+  @Get(':cod')
+  public async getUser(@Res() res, @Param('cod') cod: string) {
+    if (!cod) {
+      throw new NotFoundException('Invalid id!');
+    }
+    const user = await this.usersService.findOne(cod);
+    if (!user) {
+      throw new NotFoundException('User id does not exist!');
+    }
+    return res.status(HttpStatus.OK).json(user);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @Post()
+  public async createUser(
+    @Res() res,
+    @Body() createUserDto: CreateUserDto) {
+      try {
+        const createdUser = await this.usersService.create(createUserDto);
+    return res.status(HttpStatus.OK).json({
+      message: 'User has been submitted successfully!',
+      createdUser
+    });
+  } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        message: error.message
+      });
+    }
   }
 }
