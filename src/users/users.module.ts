32aa0c1abc5001df697de877d58ef97112659fbd
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-11-30 00:34:53 -0300

    feat: add moongose module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersSchema, Users } from './schemas/users.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Users.name, schema: UsersSchema },
+    ]),
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
