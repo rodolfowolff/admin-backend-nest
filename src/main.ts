25e38252d1dbb8e1a9b0d66a744d2d173ff1aab5
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-12-08 11:19:57 -0300

    feat: add api documentation swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,15 @@ async function bootstrap() {
       enableImplicitConversion: true,
     },
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, document);
 
   app.enableCors();
   await app.listen(3001);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,7 +13,9 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto, UpdateUserDto } from './dto';
 import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
